18dc785fc09de78400a7924d20858efc9ff26b8e
tombest1 <caleb_akinola@yahoo.com>
2021-05-19 15:37:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+document.getElementById("btn1").addEventListener("click", function () {
+    var box1 = document.getElementById("test1");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Smith Erizona"
+
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+    var box1 = document.getElementById("test2");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Jane Bob"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+    var box1 = document.getElementById("test3");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Daniel Clara"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn4").addEventListener("click", function () {
+    var box1 = document.getElementById("test4");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Mary Jane"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn5").addEventListener("click", function () {
+    var box1 = document.getElementById("test5");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Patric Okon"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn6").addEventListener("click", function () {
+    var box1 = document.getElementById("test6");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Bode Thomas"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn7").addEventListener("click", function () {
+    var box1 = document.getElementById("test7");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Frank Lamerl"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn8").addEventListener("click", function () {
+    var box1 = document.getElementById("test8");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Name:Sam Shun"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+document.getElementById("btn9").addEventListener("click", function () {
+    var box1 = document.getElementById("test9");
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        this.innerText="Gift Omo"
+    } else {
+        box1.style.display = "none";
+    }
+
+});
+
+var box = document.getElementById("btn10")
+var box1 = document.getElementById("test10");
+box.addEventListener("click", function () {
+
+    if (box1.style.display == "none") {
+        box1.style.display = "block";
+        box.innerText = "Name: Tommy Holand"
+
+    } else {
+        box1.style.display = "none";
+    }
+
+});
